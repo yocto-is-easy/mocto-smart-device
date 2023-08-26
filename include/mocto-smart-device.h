c3a8a06538c273fd73bca71d9d9b95c25459ebb5
Mykhailo Didur <mikhaildidur2003@gmail.com>
2023-08-26 19:40:17 +0300

    Setup esp-idf component
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

---
+++ include/mocto-smart-device.h
@@ -0,0 +1,6 @@
+#ifndef MOCTO_SMART_DEVICE_H
+#define MOCTO_SMART_DEVICE_H
+
+void mocto_smart_device_sample(void);
+
+#endif // MOCTO_SMART_DEVICE_H
---
+++ mocto-smart-device.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "mocto-smart-device.h"
+
+void mocto_smart_device_sample(void)
+{
+    printf("Hello world from mocto-smart-device!\n");
+}
